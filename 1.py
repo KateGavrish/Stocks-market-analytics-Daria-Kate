f3a635d4354dc640e1354b6fef6f151c9fb265fd
gavrishekaterina <ketrin2303@yandex.ru>
2020-03-30 15:55:02 +0400

    1

--- 1.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('static/static_data/code_of_currency.json') as f:
-    score = f.read()
-
-resp = json.loads(score)
-for i in range(len(resp['Valuta']['Item'])):
-    print(resp['Valuta']['Item'][i]['@ID'])
