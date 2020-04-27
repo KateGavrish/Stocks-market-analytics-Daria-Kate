09883c2257bd88a8a225dd5127dfa1491a09b913
gavrishekaterina <ketrin2303@yandex.ru>
2020-04-27 11:47:48 +0400

    API

--- scripts/functions.py
+++ scripts/functions.py
@@ -13,17 +13,17 @@ def xml_to_json(xml_file):
 def daily_data_of_all_change(list_id_curr):
     dict_of_delta = dict()
     for id_curr in list_id_curr:
+        print(id_curr)
+        date1 = datetime.date(2020, 2, 28).strftime('%d/%m/%Y')
+        date2 = datetime.date(2020, 3, 28).strftime('%d/%m/%Y')
 
-        date1 = (datetime.date.today() - datetime.timedelta(days=40)).strftime('%d/%m/%Y')
-        date2 = (datetime.date.today() - datetime.timedelta(days=30)).strftime('%d/%m/%Y')
-
-        a = data_of_one_curr_for_a_per(date2, date1, id_curr)
+        a = data_of_one_curr_for_a_per(date1, date2, id_curr)
         try:
             dict_of_delta[id_curr] = str(float(a['ValCurs']['Record'][1]['Value'].replace(',', '.')) * 100 / float(a['ValCurs']['Record'][0]['Value'].replace(',', '.')) - 100)[:5]
         except Exception as e:
             print(e)
             dict_of_delta[id_curr] = 0
-
+        print(dict_of_delta)
     return dict_of_delta
 
 
